17454cf681336bbdda3d54616c86a182176cc30c
godsakani <nyonggodwill11@gmail.com>
2023-10-25 12:55:35 +0100

    Add email notification  for sign up and pass reset,

--- src/controller/users/userController.js
+++ src/controller/users/userController.js
@@ -2,22 +2,65 @@ const express = require("express");
 const { Utils } = require("../../utils/firebase_init.js");
 const FirebaseAuthError = require("firebase-admin");
 const nodemailer = require("nodemailer");
-
+const bcrypt = require("bcryptjs");
 const admin = Utils.init_files;
 
 exports.UserController = {
   register: async (req, res) => {
     try {
+      const transporter = nodemailer.createTransport({
+        service: "gmail",
+        auth: {
+          user: "company email",
+          pass: "pass",
+        },
+      });
+
       const { email, password } = req.body;
+
+      const saltRounds = await bcrypt.genSalt(10);
+      const passwordHash = await bcrypt.hashSync(password, saltRounds);
       const userResponse = await admin.auth().createUser({
         email: email,
-        password: password,
+        password: passwordHash,
         emailVerified: false,
         disabled: false,
       });
+      if (!email) {
+        res.status(401).send({ message: "Email is Required!" });
+        return;
+      }
+      if (!password) {
+        res.status(401).send({ message: "Password is Required!" });
+        return;
+      }
+      //notification to verify account
+      const link = await admin.auth().generateEmailVerificationLink(email);
+      const mailOptions = {
+        from: "Mboacare <company email>",
+        to: email,
+        subject: "Email Verification",
+        text: `Dear ${email}
+
+        Thank you for signing up Mboacare! To complete your registration and verify your email address, please click on the following link: 
+
+        ${link}
+
+        By verifying your email, you will gain full access to all the features and benefits of our platform.
+        If you did not create an account with us, please disregard this email.
+
+        If you have any questions or need further assistance, 
+        please feel free to reach out to our support team at <mboacare237@gmail.com>.
+
+        Thank you for choosing Mboacare.
+
+        Best regards,
+        Mboacare`,
+      };
+      await transporter.sendMail(mailOptions);
 
       res.status(200).send({
-        message: "Account Created Successful",
+        message: "Account Created Successful Check email to verify account!",
         user: userResponse,
         idToken: userResponse.uid,
       });
@@ -58,8 +101,20 @@ exports.UserController = {
         res.status(401).send({ message: "Invalid email" });
         return;
       }
+      if (userRecord.emailVerified == false) {
+        res.status(401).send({ message: "Email is not verified!" });
+        return;
+      }
+      if (!email) {
+        res.status(401).send({ message: "Email is Required!" });
+        return;
+      }
+      // if (!password) {
+      //   res.status(401).send({ message: "Email is not verified!" });
+      //   return;
+      // }
       // Compare the provided password with the user's stored password hash
-      // const isPasswordValid = await bcrypt.compare(
+      // const isPasswordValid = await bcrypt.compareSync(
       //   password,
       //   userRecord.passwordSalt
       // );
@@ -83,15 +138,12 @@ exports.UserController = {
 
   update: async (req, res) => {
     try {
-      const { uid, email, password, phoneNumber, displayName, photoURL } =
-        req.body;
+      const { uid, email, phoneNumber, displayName, photoURL } = req.body;
       const updateUserData = await admin.auth().updateUser(uid, {
         uid: uid,
         email: email,
-        emailVerified: true,
         phoneNumber: phoneNumber,
         displayName: displayName,
-        password: password,
         photoURL: photoURL,
       });
       res
@@ -109,30 +161,95 @@ exports.UserController = {
 
   reset_password: async (req, res) => {
     try {
-      // const transporter = nodemailer.createTransport({
-      //   service: "gmail",
-      //   auth: {
-      //     user: "",
-      //     pass: "",
-      //   },
-      // });
+      const transporter = nodemailer.createTransport({
+        service: "gmail",
+        auth: {
+          user: "company email",
+          pass: "pass",
+        },
+      });
       const email = req.body.email;
 
       const link = await admin.auth().generatePasswordResetLink(email);
 
       // Send the password reset link to the user
 
-      // const mailOptions = {
-      //   from: "Mboacare <your_email_address@gmail.com>",
-      //   to: email,
-      //   subject: "Reset your password",
-      //   text: `Click on the following link to reset your password: ${link}`,
-      // };
-      // await transporter.sendMail(mailOptions);
-      res.status(200).json({ message: "Check Reset password link!", link });
+      const mailOptions = {
+        from: "Mboacare <company email>",
+        to: email,
+        subject: "Password Reset Notification",
+        text: `Dear ${email}
+
+        We received a request to reset your password for your account. To proceed with the password reset,
+        please click on the following link: 
+
+        ${link}
+
+        If you did not initiate this request, please ignore this email. Your password will remain unchanged.
+
+        Thank you for using Mboacare.
+
+        Best regards,
+        Mboacare
+        `,
+      };
+      await transporter.sendMail(mailOptions);
+      res.status(200).json({ message: "Check email for Reset password link!" });
     } catch (error) {
       // The user does not exist or the email address is invalid
       res.status(400).json({ message: error.message });
+    }
+  },
+
+  change_password: async (req, res) => {
+    try {
+      const { uid, email, new_password } = req.body;
+      const user = await admin.auth().getUser(uid);
+      console.log(user);
+      const userEmail = await user.email;
+
+      // Compare the old password and input password.
+      // const isPasswordCorrect = await bcrypt.compareSync(
+      //   old_password,
+      //   passwordHash
+      // );
+
+      if (email != userEmail) {
+        res.status(401).send({ message: "Invalid email incorrect!" });
+        return;
+      }
+      const updateUserData = await admin.auth().updateUser(uid, {
+        uid: uid,
+        password: new_password,
+      });
+
+      res.status(200).send({
+        message: "password changed successful!",
+        data: updateUserData,
+      });
+      // res.json({
+      //   message: "Account Updated Successful",
+      //   user: updateUserData,
+      // });
+    } catch (err) {
+      console.error(err.code, err.message);
+      res.status(401).send({ message: err.message });
+    }
+  },
+
+  log: async (req, res) => {
+    try {
+      const { uid } = req.body;
+      const data = await admin.auth().getUser(uid);
+      const token = await admin.auth().createCustomToken(data.uid);
+      const user = await admin.auth().verifySessionCookie(token);
+      res.status(200).send({
+        message: "login successful!",
+        data: user,
+      });
+    } catch (error) {
+      console.error(error.code, error.message);
+      res.status(401).send({ message: error.message });
     }
   },
 };
