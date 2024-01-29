0b2c46e7dcf8af2cc677ce5cfa1a1fa50b8dd2f1
godsakani <nyonggodwill11@gmail.com>
2024-01-29 23:35:01 +0100

    Fixed auth endpoint for profile update

--- api/src/routes/authRouts.js
+++ api/src/routes/authRouts.js
@@ -6,7 +6,7 @@ const { UserController } = require("../controller/users/userController.js");
 //Auth endpoints
 router.post("/register", UserController.register);
 router.post("/sign-in", UserController.login);
-router.put("/update-profile", UserController.update);
+router.put("/update-profile", UserController.update_profile);
 router.put("/change-password", UserController.change_password);
 router.post("/reset-password", UserController.reset_password);
 router.post("/send-link", UserController.sendlink);
